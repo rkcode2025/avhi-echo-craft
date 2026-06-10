fad349264a9507ef351891621786de2d18a7034b
Syphax <ss3240069@gmail.com>
2026-06-10 22:20:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { nitro } from "nitro/vite"; // <--- Add this import
 
 export default defineConfig({
-  plugins: [
-    tanstackStart(), 
-    viteReact(),
-    nitro({ preset: "vercel" }) // <--- Force Vercel output here
-  ],
+  vite: {
+    plugins: [
+      nitro({ preset: "vercel" }) // <--- Force Vercel output here
+    ],
+  },
 });
